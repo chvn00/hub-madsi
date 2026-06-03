b9a0c1eb5e9200d8437dce9a2dc431925beca62e
Cesar Valencia <cesarvalencia@Mac-mini-de-Cesar.local>
2026-06-03 15:22:55 -0500

    Agregar logo USTA y corregir ruta de imagen

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return send_from_directory(".", "index.html")
 
+@app.route("/escudo.png")
+def logo():
+    return send_from_directory(".", "escudo.png")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
